19bb66c67e236f2886ec53bf1fe98b6e3715dc54
s.slonchakov <slonchakov@gmail.com>
2023-04-05 22:03:41 +0300

    add endpoints

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,15 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->group(['prefix' => 'api/user'], function () use ($router) {
+    $router->post('sign-in', 'AuthController@signIn');
+    $router->post('register', 'AuthController@register');
+    $router->post('recover-password', 'AuthController@recoverPassword');
+    $router->post('update-password', 'AuthController@updatePassword');
+
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        $router->get('companies', 'CompanyController@get');
+        $router->post('companies', 'CompanyController@create');
+    });
 });
